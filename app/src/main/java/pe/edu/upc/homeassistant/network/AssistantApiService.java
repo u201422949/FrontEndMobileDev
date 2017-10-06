85776b79f8a914cc2cdff12513a94b01696b36f1
pcabrerahundred <paul.cabrera@hundred.com.pe>
2017-10-06 18:05:47 -0500

    Fix error merge

--- app/src/main/java/pe/edu/upc/homeassistant/network/AssistantApiService.java
+++ app/src/main/java/pe/edu/upc/homeassistant/network/AssistantApiService.java
@@ -5,13 +5,10 @@ package pe.edu.upc.homeassistant.network;
  */
 
 public class AssistantApiService {
-
     public static String LOGIN_CLIENT_URL = "http://45.63.21.47/login";
-<<<<<<< HEAD
     public static String REGISTER_CLIENT_URL = "http://45.63.21.47/client/register";
-=======
-    public static String REGISTER_CLIENT_URL = "";
+
+    //public static String REGISTER_CLIENT_URL = "";
     public static String GET_EXPERTS_URL = "";
     public static String SEND_REQUEST_BUDGET_URL = "";
->>>>>>> a115a5d8ea5452d1d2e874edeac7764f87f3f577
 }
